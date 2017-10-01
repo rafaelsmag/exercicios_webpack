77e9a297858d032d145c653f430af6f0046be267
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-10-01 17:47:39 -0300

    Adding babel

--- ex/index.js
+++ ex/index.js
@@ -1,1 +1,15 @@
-console.log('webpack')
+const produto = {
+  nome: 'Caneta',
+  preco: 1.25,
+  desconto: 0.25,
+}
+
+function clone(obj) {
+  return { ...obj }
+}
+
+const novoProduto = clone(produto)
+novoProduto.nome = 'Lapis'
+
+console.log(novoProduto)
+console.log(produto)
--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,15 @@ module.exports = {
     port: 8080,
     contentBase: './public',
   },
+  module: {
+    loaders: [{
+      test: /.js?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015'],
+        plugins: ['transform-object-rest-spread'],
+      },
+    }],
+  },
 }
